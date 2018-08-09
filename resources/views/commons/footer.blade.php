f833922409145fc9af0deef8ff339c3d4ebc49be
Tatsuya Fujii <sekkatinakatatumuri@gmail.com>
2018-08-09 05:32:03 +0000

    login, register

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <div class="text-center text-muted">© 2018 まるトラ.</div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index')->name('welcome.index');
 
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
